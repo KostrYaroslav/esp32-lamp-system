a328c364a56aecebcd7af1a6ece3f510174f0f4c
KostrYaroslav <slavapavlovsk3@gmail.com>
2026-05-19 12:29:13 +0900

    Replace zigbee_coordinator with matter_gateway

--- zigbee_coordinator/main/command_parser.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void parser_init(void);
-void parser_start_task(void);
--- zigbee_coordinator/main/uart_handler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-#include <stdint.h>
-
-void uart_init(void);
-void uart_start_rx_task(void);
-void uart_send_command(const char *cmd);
-bool uart_request_state(void);
