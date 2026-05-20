0e885a1ab2fe612ad13d6e5bcdc0eacfdfc21d70
KostrYaroslav <slavapavlovsk3@gmail.com>
2026-05-20 12:58:42 +0900

    Заново сделал matter

--- matter_gateway/main/uart_handler.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void uart_init(void);
-void uart_send_command(const char *cmd);
-void uart_start_rx_task(void);
---
+++ matter_gateway/matter_gateway.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "matter_gateway.h"
+
+void func(void)
+{
+
+}
